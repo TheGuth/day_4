9a1c19f372dfd0405d5dc467217667d37aeb2f8c
James Guthrie <jamesrguthrie@gmail.com>
2017-02-02 20:29:25 -0500

    Added Classes to Index file and Finished Jquery to append analyzing variables to the DOM

--- challeng_text_analyzer/app.js
+++ challeng_text_analyzer/app.js
@@ -25,13 +25,14 @@
     // total += sentence.length
   // total / sentence array .length
 
-  // word count: 113; Done
+  // word count: 113; Done actual answer 130;
 
-  // unique word count: 83;
+  // unique word count: 83; Done actual answer 88;
 
-  // average word length: 4.91;
+  // average word length: 4.91; Done actual answer 4.346
 
   // average sentence length: 113.00 words; Based off periods for sentences
+    // Done actual answer 130;
 
 // function appender(textToAnalyze) {
 //    
@@ -40,73 +41,84 @@
 $(function() {
 
   $('button').click(function(event) {
+    event.preventDefault();
+    $("dl").removeClass('hidden');
+
+    // capturing text from textarea and then turning it into
+    // an array breaking it up by new line character.
+
     var textToAnalyze = $('textarea').val();
     textToAnalyze = textToAnalyze.split('\n');
 
-    var wordCount = textToAnalyze.join(' ').split(' '); // .length
+    // variables holding word array, and sentence array
 
+    var wordCount = textToAnalyze.join(' ').split(' '); 
     var sentencesCount = textToAnalyze.join(' ').split('.');
 
+    // Variables being set to the function calls to answer each
+    // analyzing question.
+
     var WordCountNumber = textToAnalyze.join(' ').split(' ').length;
-
     var uniqueWordCount = uniqueWords(wordCount);
-
     var averageWordLengthCount = averageWordLength(wordCount);
+    var averageSentenceCount = averageSentenceLength(sentencesCount);
 
-    var averageSentenceCount = averageSentence(sentencesCount);
+    // Appending to the DOM through classes set on Definition 
+    // description elements
 
-
+    $(".js-wordCount").text(WordCountNumber);
+    $(".js-uniqueWordCount").text(uniqueWordCount);
+    $(".js-averageWordLength").text(averageWordLengthCount);
+    $(".js-averageSentenceLength").text(averageSentenceCount);
 
   });
 
-function uniqueWords(wordCount) {
-  var newWords = [];
-  for(var i = 0; i < wordCount.length; i++) {
-    if(newWords.includes(wordCount[i])) {
+  function uniqueWords(wordCount) {
+    var newWords = [];
+    for(var i = 0; i < wordCount.length; i++) {
+      if(newWords.includes(wordCount[i])) {
 
+      }
+      else {
+        newWords.push(wordCount[i]);
+      }
     }
-    else {
-      newWords.push(wordCount[i]);
-    }
+    
+    return newWords.length;
   }
-  console.log(newWords.length);
-  return newWords.length;
-}
 
-// function averageWord(word array) 
-//   var total = 0;
-//   iterate over the array
-//     total += word.length
-//   total / word array .length
+  function averageWordLength(words) {
+    var total = 0;
+    words.forEach(function(word) {
+      total += word.length;
+    });
 
-function averageWordLength(words) {
-  var total = 0;
-  words.forEach(function(word) {
-    total += word.length;
-  });
+    return (total / words.length).toFixed(2);
+  }
 
-  console.log(total / words.length);
-  return total / words.length;
-}
+  function averageSentenceLength(sentences) {
+    var sentenceLength = 0;
+    sentences.forEach(function(sentence) {
+      sentenceLength += sentence.split(' ').length;
+    });
 
+    return (sentenceLength / sentences.length).toFixed(2);
+  }
 
-function averageSentence(sentences) {
-  var total = 0;
+  // function averageSentence(sentences) {
+  //   var total = 0;
 
-  // console.log(sentences.length);
-  sentences.forEach(function(sentence) {
-    total += sentence.length;
-  // console.log(sentences.length);
-  });
+  //   // console.log(sentences.length);
+  //   sentences.forEach(function(sentence) {
+  //     total += sentence.length;
+  //   // console.log(sentences.length);
+  //   });
 
-  console.log(total);
-  console.log(sentences.length);
-  console.log(total / sentences.length);
-  return total / sentences.length;
-}
-
-
-
+  //   console.log(total);
+  //   console.log(sentences.length);
+  //   console.log(total / sentences.length);
+  //   return total / sentences.length;
+  // }
 });
 
 
